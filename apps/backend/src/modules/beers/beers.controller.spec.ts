f67cf48075afa0d315f778552913ab8ec94e29d2
Leander P <246755970+LeFlunder@users.noreply.github.com>
2026-09-12 16:12:06 +0200

    fix:test after update of business logic

--- apps/backend/src/modules/beers/beers.controller.spec.ts
+++ apps/backend/src/modules/beers/beers.controller.spec.ts
@@ -37,6 +37,7 @@ describe('BeersController (integration, mocked DB)', () => {
     where: jest.fn().mockReturnThis(),
     andWhere: jest.fn().mockReturnThis(),
     orderBy: jest.fn().mockReturnThis(),
+    addOrderBy: jest.fn().mockReturnThis(),
     skip: jest.fn().mockReturnThis(),
     take: jest.fn().mockReturnThis(),
     getManyAndCount: jest.fn(),
